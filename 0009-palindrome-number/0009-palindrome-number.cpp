8a0efde39d0056684605770cff007ae3490abf50
Mansi Kumari <162333120+Mansiiiiiiiiiiiiiiiii@users.noreply.github.com>
2025-04-06 16:48:44 +0530

    Time: 4 ms (34.2%), Space: 8.5 MB (69.03%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -4,14 +4,14 @@ public:
       if (x<0){
         return false;
       }
-      int original = x;
+      int x1 = x;
       long reverse;
       while(x !=0){
         int digit = x%10;//121%10=1
         reverse = reverse*10+ digit;//
         x=x/10;
       }
-      return original==reverse;
+      return x1==reverse;
     }
         
     
