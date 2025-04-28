82bf17c7484707a8ecf904458e01bd0245ddeefc
Mansi Kumari <162333120+Mansiiiiiiiiiiiiiiiii@users.noreply.github.com>
2025-04-28 18:51:48 +0530

    Time: 0 ms (100%), Space: 11.4 MB (88.2%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         int n = digits.size();
-        for(int i=n-1; i >= 0; i--){
+        for(int i=n-1; i >= 0; i--){//Travese from the last element
             if(digits[i]<9){
+                // Agar current element is than 9 , hust add 1 and return 
                 digits[i]++;
                 return digits;
             }
