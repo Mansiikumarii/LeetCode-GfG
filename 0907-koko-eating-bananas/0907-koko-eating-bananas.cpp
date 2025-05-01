eb870864017b991a0f1ace92b463d6c2a14f8fa0
Mansi Kumari <162333120+Mansiiiiiiiiiiiiiiiii@users.noreply.github.com>
2025-05-01 15:27:38 +0530

    Time: 7 ms (75.66%), Space: 22.9 MB (74.2%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPossible(long long mid, vector<int>& piles, int h) {
+        long long time = 0;
+        for (int pile : piles) {
+            time += (pile + mid - 1) / mid;  // ceiling division
+        }
+        return time <= h;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        long long low = 1;
+        long long high = *max_element(piles.begin(), piles.end());
+        long long ans = high;
+
+        while (low <= high) {
+            long long mid = low + (high - low) / 2;
+            if (isPossible(mid, piles, h)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return static_cast<int>(ans);
+    }
+};
