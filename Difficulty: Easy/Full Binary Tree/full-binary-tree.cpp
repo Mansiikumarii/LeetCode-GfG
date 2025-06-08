46fe2f3f8a9d1615430fdacd6a44a6f872e09bb0
Mansi Kumari <162333120+Mansiiiiiiiiiiiiiiiii@users.noreply.github.com>
2025-06-08 15:51:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Full Binary Tree/full-binary-tree.cpp
@@ -0,0 +1,27 @@
+/*Complete the function below
+Node is as follows:
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+// Return True if the given Binary Tree is a Full Binary Tree. Else return False
+bool isFullTree(struct Node* root) {
+    // add code here.
+    if(root ==NULL)
+    return true;
+    if(root->left == NULL && root->right ==NULL)
+    return true;
+    
+    if(root->left && root->right)
+    return isFullTree(root->left) && isFullTree(root->right);
+    
+    // If one child is null and the other is not
+    return false;
+}
