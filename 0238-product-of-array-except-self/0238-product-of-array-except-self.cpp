db7515a409b90b7c0d6049004ab74d2a7c3d3fcd
Mansi Kumari <162333120+Mansiiiiiiiiiiiiiiiii@users.noreply.github.com>
2025-06-16 21:52:41 +0530

    Time: 1 ms (51.77%), Space: 40.2 MB (79.42%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
+
         int n= nums.size();
-        vector<int> ans(n,1);
-        for(int i=1; i< n; i++){
-            ans[i]=ans[i-1]*nums[i-1];
+        vector<int> answer(n,1);
+
+        for(int i=1; i<n; i++){
+            answer[i] = answer[i-1]*nums[i-1];
         }
-        int suffix = 1;
+        int suffix =1;
         for(int i=n-1; i>=0; i--){
-            ans[i] *= suffix;
+            answer[i] *=suffix;
             suffix *= nums[i];
         }
-        return ans;
+        return answer;
     }
 };
