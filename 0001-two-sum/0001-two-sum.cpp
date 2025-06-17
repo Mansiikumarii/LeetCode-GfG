81d44ca9063f63ab5ce32f0e1ab3827f7ab09d59
Mansi Kumari <162333120+Mansiiiiiiiiiiiiiiiii@users.noreply.github.com>
2025-06-17 11:10:21 +0530

    Time: 4 ms (57.58%), Space: 14.9 MB (21.94%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -13,16 +13,30 @@ public:
         // }
         // return{-1,-1};
 
-//Brute force approach
-        int n= nums.size();
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(i == j) continue;
-                if(nums[i]+nums[j] == target){
-                   return {i, j};
-                }
-            }
+        int n=nums.size();
+        unordered_map<int,int>mp;
+      
+
+       for(int i=0; i<n; i++){
+        int req = target - nums[i];
+
+        if(mp.count(req)){
+            return{i,mp[req]};
         }
-        return {};
+        mp[nums[i]] = i;
+       }
+       return {-1,-1};
+
+//Brute force approach
+        // int n= nums.size();
+        // for(int i=0; i<n; i++){
+        //     for(int j=0; j<n; j++){
+        //         if(i == j) continue;
+        //         if(nums[i]+nums[j] == target){
+        //            return {i, j};
+        //         }
+        //     }
+        // }
+        // return {};
     }
 };
