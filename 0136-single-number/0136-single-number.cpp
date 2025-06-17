79bb9882f5dbe030c254065c686eac1b3ad59371
Mansi Kumari <162333120+Mansiiiiiiiiiiiiiiiii@users.noreply.github.com>
2025-06-17 10:05:23 +0530

    Time: 5 ms (22.36%), Space: 20.7 MB (58.65%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int result=0;
-        for(int i=0; i< nums.size(); i++){
-            result ^= nums[i];
-        }
-        return result;
-
+       
+sort(nums.begin(), nums.end());
         
+        for(int i=0; i<nums.size()-1; i+=2){
+            if (nums[i] != nums[i + 1]) {
+                return nums[i]; // This number doesn't have a pair
+            }
+            
+        }
+        
+         return nums[nums.size() - 1];
     }
 };
