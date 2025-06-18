b9223cbbb6a5acf6317b9f253b26aecbb3ad9c41
Mansi Kumari <162333120+Mansiiiiiiiiiiiiiiiii@users.noreply.github.com>
2025-06-18 09:28:25 +0530

    Time: 2 ms (38.77%), Space: 10.4 MB (18.26%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,34 +1,48 @@
 class Solution {
 public:
-bool isAlphaNumeric(char c){
-if((c >='0' && c<='9')|| (c>='a' && c<='z')|| (c>='A' && c<= 'Z')){
-    return true;}
-    return false;
-}
+// bool isAlphaNumeric(char c){
+// if((c >='0' && c<='9')|| (c>='a' && c<='z')|| (c>='A' && c<= 'Z')){
+//     return true;}
+//     return false;
+// }
     bool isPalindrome(string s) {
-        int start = 0, end = s.size() - 1;
-        while(start<end){
-           if (!isAlphaNumeric(s[start]))
-{
-                start++; continue;
-            }
-           if (!isAlphaNumeric(s[end]))
-{
-                end--; continue;
-            } 
-            if(tolower(s[start]) != tolower(s[end])){
-                return false;
-            }
-            start++;
-             end--;
+//         int start = 0, end = s.size() - 1;
+//         while(start<end){
+//            if (!isAlphaNumeric(s[start]))
+// {
+//                 start++; continue;
+//             }
+//            if (!isAlphaNumeric(s[end]))
+// {
+//                 end--; continue;
+//             } 
+//             if(tolower(s[start]) != tolower(s[end])){
+//                 return false;
+//             }
+//             start++;
+//              end--;
         
         
+//         }
+//         return true;
+//     }
+
+string temp = "";
+        for(auto x: s){
+            if(isalnum(x)) temp += tolower(x);
         }
+ 
+        int i = 0, j = temp.size() - 1;
+ 
+        while(i < j){
+            if(temp[i] != temp[j]) return false;
+ 
+            i++;
+            j--;
+        }
+ 
         return true;
-    }
-
-    
-
+}
 };
 
     
