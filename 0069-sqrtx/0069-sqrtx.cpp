f5d918c616c066eb54d0351c6c826bec16ceaa06
Mansi Kumari <162333120+Mansiiiiiiiiiiiiiiiii@users.noreply.github.com>
2025-06-19 10:26:48 +0530

    Time: 0 ms (100%), Space: 8.5 MB (50.88%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
     int mySqrt(int x) {
-      int low =0;
-      int high = x;
-      int ans =-1;
+    //   int low =0;
+    //   int high = x;
+    //   int ans =-1;
 
-      while(low<= high){
-        long long mid= low+(high-low)/2;
+    //   while(low<= high){
+    //     long long mid= low+(high-low)/2;
 
-        if(mid*mid == x) return mid;
+    //     if(mid*mid == x) return mid;
 
-        else if(mid*mid > x){
-            high = mid -1;
+    //     else if(mid*mid > x){
+    //         high = mid -1;
+    //     }
+    //     else{
+    //         low = mid +1;
+    //         ans = mid;
+    //     }
+    //   }  
+    //   return ans;
+    // }
+
+    
+        if (x == 0) return 0;
+        long long guess = x;
+        while (guess * guess > x) {
+            guess = (guess + x / guess) / 2;
         }
-        else{
-            low = mid +1;
-            ans = mid;
-        }
-      }  
-      return ans;
+        return guess;
     }
 };
