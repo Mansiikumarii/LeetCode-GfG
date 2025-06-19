cc32dfaa2e4847d45912df873c6c9d594ae3bdf7
Mansi Kumari <162333120+Mansiiiiiiiiiiiiiiiii@users.noreply.github.com>
2025-06-19 17:55:48 +0530

    Time: 0 ms (100%), Space: 13.6 MB (77.03%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -3,24 +3,43 @@ public:
     int searchInsert(vector<int>& nums, int target) {
         //  return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
 
-        int n = nums.size();
-        int low =0;
-        int high = n;
+        // int n = nums.size();
+        // int low =0;
+        // int high = n;
 
-        if(nums[n-1]<target) return n;
+        // if(nums[n-1]<target) return n;
 
-        while(low<= high){
-            int mid = low+(high-low)/2;
+        // while(low<= high){
+        //     int mid = low+(high-low)/2;
 
-            if(nums[mid] == target) return mid;
+        //     if(nums[mid] == target) return mid;
 
-            if(target <nums[mid]){
-                high = mid-1;
-            }
-            else{
-                low = mid+1;
-            }
-        }
-        return low;
+        //     if(target <nums[mid]){
+        //         high = mid-1;
+        //     }
+        //     else{
+        //         low = mid+1;
+        //     }
+        // }
+        // return low;
+
+      int n=nums.size();
+      int low=0;
+      int high =n;
+
+      if(nums[n-1]<target) return n;
+
+      while(low<=high){
+        int mid = low+(high-low)/2;
+
+        if(nums[mid] == target ) return mid;
+
+        if(target < nums[mid])
+        high = mid-1;
+
+        else
+        low = mid+1;
+      }
+      return low;
     }
 };
