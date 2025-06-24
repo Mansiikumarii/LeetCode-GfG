eba75e65ee022b86b4a43b701902fd3bf7cea8fc
Mansi Kumari <162333120+Mansiiiiiiiiiiiiiiiii@users.noreply.github.com>
2025-06-24 10:56:32 +0530

    Time: 15 ms (12.22%), Space: 14.6 MB (11.32%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,23 +9,23 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        // unordered_set<ListNode *>s;
-        // ListNode *curr = head;
-        // while(curr){
-        //     if(s.find(curr) != s.end()) return true;
-        //     s.insert(curr);
-        //     curr = curr->next;
-        // }
-        // return false;
+        unordered_set<ListNode *>s;
+        ListNode *curr = head;
+        while(curr){
+            if(s.find(curr) != s.end()) return true;
+            s.insert(curr);
+            curr = curr->next;
+        }
+        return false;
 
         //By Floyeds cycle detection algo
-        ListNode *slow = head;
-        ListNode *fast = head;
-        while(fast!= NULL and fast->next != NULL){
-            slow = slow->next;
-            fast = fast->next->next;
-            if(slow == fast) return true;
-        } 
-        return false;
+        // ListNode *slow = head;
+        // ListNode *fast = head;
+        // while(fast!= NULL and fast->next != NULL){
+        //     slow = slow->next;
+        //     fast = fast->next->next;
+        //     if(slow == fast) return true;
+        // } 
+        // return false;
     }
 };
