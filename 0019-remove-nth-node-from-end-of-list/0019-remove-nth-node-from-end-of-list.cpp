1347768a27f3435dadcf5f9851093d16330ffd0e
Mansi Kumari <162333120+Mansiiiiiiiiiiiiiiiii@users.noreply.github.com>
2025-06-24 20:12:35 +0530

    Time: 0 ms (100%), Space: 15.1 MB (24.55%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,33 +11,57 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        int cnt =0;
-        ListNode* temp = head;
+        // Brute force approach
+    //     int cnt =0;
+    //     ListNode* temp = head;
 
-        // Count total number of nodes
-        while(temp != NULL){
-            cnt++;
-            temp = temp->next;
-        }
-        if(cnt == n){
-         ListNode* newHead = head->next;
+    //     // Count total number of nodes
+    //     while(temp != NULL){
+    //         cnt++;
+    //         temp = temp->next;
+    //     }
+    //     if(cnt == n){
+    //      ListNode* newHead = head->next;
           
-            return newHead;
-        }
+    //         return newHead;
+    //     }
 
-         // Find the (cnt - n)th node
-        int res = cnt - n;
-        temp = head;
-        while(--res>0){
+    //      // Find the (cnt - n)th node
+    //     int res = cnt - n;
+    //     temp = head;
+    //     while(--res>0){
            
-            temp = temp->next;
+    //         temp = temp->next;
 
-    }
-      // Delete the node
-    ListNode* delNode = temp->next;
+    // }
+    //   // Delete the node
+    // ListNode* delNode = temp->next;
    
-    temp->next = temp->next->next;
-    delete delNode;
-    return head;
+    // temp->next = temp->next->next;
+    // delete delNode;
+    // return head;
+ListNode* dummy = new ListNode(0,head);
+ListNode* fast = dummy;
+ListNode* slow = dummy;
+
+
+for(int i=0; i<= n; i++){
+    fast = fast->next;
+}
+
+ // Move both fast and slow until fast reaches the end
+while(fast != NULL){
+    slow = slow->next;
+    fast = fast->next;
+}
+// Now slow->next is the node to be deleted
+ListNode* delNode = slow->next;
+slow->next= slow->next->next;
+delete delNode;
+
+ListNode* newHead = dummy->next;
+delete dummy;
+return newHead;
+
     }  
 };
