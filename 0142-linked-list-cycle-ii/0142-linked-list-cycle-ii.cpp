bb0c96ff02664fd44504a9b8b33c13aaf2f29619
Mansi Kumari <162333120+Mansiiiiiiiiiiiiiiiii@users.noreply.github.com>
2025-06-24 11:32:30 +0530

    Time: 3 ms (98.25%), Space: 11.4 MB (53.63%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,23 +9,48 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        ListNode *slow = head;
-        ListNode *fast = head;
-        while(fast and fast->next){ //or nahi likheneg bcz null ka next error dega
-            slow = slow->next;
-            fast = fast->next->next;
-            if(slow == fast ) break; //linked ke andar cycle hai
-        }
-        if(fast == NULL or fast->next == NULL) return NULL; //cycle nahi hai
-        //yahan or hai agar dono main koi ek null hoga toh condistion true hogi
-        slow = head; // slow pointer ko starting pe le ao
-        //ek ek step se padhao
-        while(slow != fast){
-            slow = slow->next;
-            fast = fast->next;
-        }
-        return slow;
+        // ListNode *slow = head;
+        // ListNode *fast = head;
+        // while(fast and fast->next){ //or nahi likheneg bcz null ka next error dega
+        //     slow = slow->next;
+        //     fast = fast->next->next;
+        //     if(slow == fast ) break; //linked ke andar cycle hai
+        // }
+        // if(fast == NULL or fast->next == NULL) return NULL; //cycle nahi hai
+        // //yahan or hai agar dono main koi ek null hoga toh condistion true hogi
+        // slow = head; // slow pointer ko starting pe le ao
+        // //ek ek step se padhao
+        // while(slow != fast){
+        //     slow = slow->next;
+        //     fast = fast->next;
+        // }
+        // return slow;
 
-        //Linked List Cycle 2 proof
+        // //Linked List Cycle 2 proof
+
+     ListNode* slow = head;
+     ListNode* fast = head;
+
+     while(fast && fast->next){
+        slow= slow->next;
+        fast = fast->next->next;
+
+        if(fast== slow) break;
+     }
+
+     if(fast == nullptr || fast->next == nullptr) return nullptr;
+     fast = head;
+
+     while(fast != slow){
+        slow= slow->next;
+        fast = fast->next;
+     }
+return fast;
+
+
+
+
+
+
     }
 };
