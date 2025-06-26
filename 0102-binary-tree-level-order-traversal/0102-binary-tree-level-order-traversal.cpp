2e24bf5a69072511147027787a8c03663074bd89
Mansi Kumari <162333120+Mansiiiiiiiiiiiiiiiii@users.noreply.github.com>
2025-06-26 18:28:44 +0530

    Time: 3 ms (28.26%), Space: 17 MB (88.41%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -12,22 +12,54 @@
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>>ans; //2-D vector ke andar sirf vector push kar sakte hai
+        // vector<vector<int>>ans; //2-D vector ke andar sirf vector push kar sakte hai
+        // if(root == NULL) return ans;
+        // queue<TreeNode *>q;
+        // q.push(root);
+        // while(!q.empty()){
+        //     int size = q.size();
+        //     vector<int> temp;
+        //     for(int i=0; i<size; i++){
+        //         TreeNode *node = q.front();
+        //         q.pop();
+        //         temp.push_back(node->val);
+        //         if(node->left) q.push(node->left);
+        //         if(node->right) q.push(node->right);
+        //     }
+        //     ans.push_back(temp);
+        // } 
+        // return ans;
+
+
+
+
+
+
+
+
+
+
+
+
+
+        vector<vector<int>>ans;
         if(root == NULL) return ans;
-        queue<TreeNode *>q;
+        queue<TreeNode* > q;
         q.push(root);
         while(!q.empty()){
-            int size = q.size();
-            vector<int> temp;
-            for(int i=0; i<size; i++){
+            int n = q.size();
+            vector<int>temp;
+            for(int i=0; i<n; i++){
                 TreeNode *node = q.front();
                 q.pop();
                 temp.push_back(node->val);
-                if(node->left) q.push(node->left);
-                if(node->right) q.push(node->right);
+                if(node->left) 
+                q.push(node->left);
+                if(node->right)
+                q.push(node->right);
             }
             ans.push_back(temp);
-        } 
+        }
         return ans;
     }
 };
