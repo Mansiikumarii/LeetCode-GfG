c3871964d21fadcd857916fd474bd589491ba81f
Mansi Kumari <162333120+Mansiiiiiiiiiiiiiiiii@users.noreply.github.com>
2025-07-01 09:56:05 +0530

    Time: 0 ms (100%), Space: 8.7 MB (18.13%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+int recursion(int n, vector<int>&dp){
+    if(n==0) return 1;
+    if(n<0) return 0;
+    if(dp[n] != -1) return dp[n];
+
+    int one = recursion(n-1, dp);
+    int two = recursion(n-2,dp);
+    return dp[n]= one+two;
+}
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+        return recursion(n,dp);
+    }
+};
