243aa8be0e96731ba0e4c79744ae0c4647ebfa3b
Mansi Kumari <162333120+Mansiiiiiiiiiiiiiiiii@users.noreply.github.com>
2025-07-01 10:30:12 +0530

    Time: 14 ms (22.27%), Space: 7.9 MB (36.4%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -15,10 +15,31 @@ public:
     //     b=nextTerm;
     //   }
     //   return b;
-
+//recursive
  if(n==0) return 0;
  if(n==1) return 1;
  return fib(n-1)+ fib(n-2) ;
+
+//tabular
+//         vector<int>dp(n+1,0);
+
+//         if(n == 0) return 0;
+//         if(n == 1) return 1;
+
+//         dp[0] = 0;
+//         dp[1] = 1;
+
+//         for(int i = 2;i <= n;i++){
+
+//             dp[i] = dp[i-1]+dp[i-2];
+//         }
+
+//         return dp[n];
+
+
+
+
+
 
 // if(n==0) return 0;
 //    if(n==1) return 1;
