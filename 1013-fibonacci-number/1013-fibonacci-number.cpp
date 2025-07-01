d8baa385c280887e65011d1b5e18114793f0716f
Mansi Kumari <162333120+Mansiiiiiiiiiiiiiiiii@users.noreply.github.com>
2025-07-01 10:22:23 +0530

    Time: 11 ms (37.62%), Space: 7.8 MB (36.4%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int fib(int n) {
+        //Itrative
     //   if(n<0){
     //     return n;
     //   }
@@ -15,29 +16,23 @@ public:
     //   }
     //   return b;
 
-  
-if(n==0) return 0;
-   if(n==1) return 1;
-   int prevprev =0;
-   int prev = 1;
-   int ans;
+ if(n==0) return 0;
+ if(n==1) return 1;
+ return fib(n-1)+ fib(n-2) ;
 
-   for(int i=2;i<=n; i++ ){
-    ans = prevprev+prev;
-    prevprev = prev;
-    prev= ans;
-       }
+// if(n==0) return 0;
+//    if(n==1) return 1;
+//    int prevprev =0;
+//    int prev = 1;
+//    int ans;
 
-return ans;
+//    for(int i=2;i<=n; i++ ){
+//     ans = prevprev+prev;
+//     prevprev = prev;
+//     prev= ans;
+//        }
 
-
-
-
-
-
-
-
-
+// return ans;
 
     }
 };
