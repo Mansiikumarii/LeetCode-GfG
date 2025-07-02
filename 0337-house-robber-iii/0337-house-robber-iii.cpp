5673a89e5799bb4dea696a3f096e547440b43124
Mansi Kumari <162333120+Mansiiiiiiiiiiiiiiiii@users.noreply.github.com>
2025-07-02 18:25:18 +0530

    Time: 13 ms (17.23%), Space: 33.1 MB (5.13%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.cpp
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -11,26 +11,18 @@
  */
 class Solution {
 public:
-vector<int>recursion(TreeNode* root){
-    if(!root) return{0,0};
-
-    vector<int>left(2,0), right(2,0); //left right ke liye 2 size ka
-    left = recursion(root->left);
-    right = recursion(root->right);
-    vector<int>curr(2,0);
-    curr[0] = max(left[0], left[1])+ max(right[0], right[1]);
-    curr[1] = root->val+ right[0]+ left[0];
-
-    return curr;
+vector<int> travel(TreeNode* root){
+     if(root == NULL) return {0,0};
+     vector<int>left = travel(root->left);
+    vector<int>right = travel(root->right);
+    vector<int>options(2,0);
+    options[0] = root->val + left[1] + right[1];
+    options[1] = max(left[0], left[1])+ max(right[0],right[1]);
+    return options; 
 }
     int rob(TreeNode* root) {
-        //0 ka matlb hai no robbed
-        //1 ka matlab robbed
+       vector<int> result = travel(root); 
+       return max(result[0],result[1]);
 
-        vector<int>dp(2,0);
-
-        dp = recursion(root);
-
-        return max(dp[0], dp[1]); //nahi chori and chori
     }
 };
