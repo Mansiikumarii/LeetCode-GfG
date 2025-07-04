2a2f75ea2efe3d3cd87925d92478b860986e609f
Mansi Kumari <162333120+Mansiiiiiiiiiiiiiiiii@users.noreply.github.com>
2025-07-04 05:49:12 +0530

    Time: 0 ms (100%), Space: 97.3 MB (60.31%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -11,14 +11,15 @@ public:
     //    }
     //    return ans;
 
-int mini = prices[0];
-int profit = 0;
-int n = prices.size();
-for(int i=1; i<n; i++){
-    int cost = prices[i] - mini;
-    profit = max(profit, cost);
-    mini = min(mini,prices[i]);
-}
-return profit;
+
+    int n= prices.size();
+    int profit =0;
+    int mini = prices[0];
+    for( int i=1; i<n; i++){
+        int cost = prices[i] - mini;
+         profit = max(profit,cost);
+        mini = min(mini, prices[i]);
     }
+    return profit;
+     }
 };
