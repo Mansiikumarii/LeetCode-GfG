7b8070860ebb308685e33ce271036498c4c16daf
Mansi Kumari <162333120+Mansiiiiiiiiiiiiiiiii@users.noreply.github.com>
2025-07-20 01:22:10 +0530

    Time: 43 ms (28.9%), Space: 14.1 MB (63.84%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,42 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // map<int,int>mpp;
-        // int n = nums.size();
-        // for(int i=0; i<n;i++){
-        //     int num = nums[i];
-        //     int moreNeeded = target - num;
-        //     if(mpp.find(moreNeeded) != mpp.end()){
-        //         return{mpp[moreNeeded],i};
-        //     }
-        //     mpp[num]=i;
-        // }
-        // return{-1,-1};
-
-        int n=nums.size();
-        unordered_map<int,int>mp;
-      
-
-       for(int i=0; i<n; i++){
-        int req = target - nums[i];
-
-        if(mp.count(req)){
-            return{i,mp[req]};
+        for(int i=0; i<nums.size(); i++){
+            for(int j = i+1; j<nums.size(); j++){
+                if(nums[i] + nums[j] == target){
+                    return {i, j};
+                }
+            }
         }
-        mp[nums[i]] = i;
-       }
-       return {-1,-1};
-
-//Brute force approach
-        // int n= nums.size();
-        // for(int i=0; i<n; i++){
-        //     for(int j=0; j<n; j++){
-        //         if(i == j) continue;
-        //         if(nums[i]+nums[j] == target){
-        //            return {i, j};
-        //         }
-        //     }
-        // }
-        // return {};
+        return {};
     }
 };
