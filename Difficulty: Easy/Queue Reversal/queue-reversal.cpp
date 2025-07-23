fe3eac1a8f75f96ea1a7fa17ff9937dd49770226
Mansi Kumari <162333120+Mansiiiiiiiiiiiiiiiii@users.noreply.github.com>
2025-07-23 19:59:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Queue Reversal/queue-reversal.cpp
@@ -0,0 +1,21 @@
+// function Template for C++
+
+class Solution {
+  public:
+    queue<int> reverseQueue(queue<int> &q) {
+        // code here.
+        
+        stack<int>st;
+        while(!q.empty()){
+            int element = q.front();
+            q.pop();
+            st.push(element);
+        }
+        while(!st.empty()){
+            int element = st.top();
+            st.pop();
+            q.push(element);
+        }
+        return q;
+    }
+};
