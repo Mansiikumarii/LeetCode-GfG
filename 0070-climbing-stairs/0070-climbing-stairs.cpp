2b67fcc3d818c13bbc1c93b54a5e222efe9aa342
Mansi Kumari <162333120+Mansiikumarii@users.noreply.github.com>
2025-08-10 14:07:05 +0530

    Time: 2 ms (5.21%), Space: 8.7 MB (18.37%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-int f(int idx, vector<int>& dp){
-    if(idx == 0) return 1;
-    if(idx <0) return 0;
+int f(int idx, vector<int>&dp){
+    if(idx ==0) return 1;
+    if(idx<1) return 0;
     if(dp[idx] != -1) return dp[idx];
-    int left = f(idx-1,dp);
-    int right = f(idx-2,dp);
-    return dp[idx] = left+right;
+    int left = f(idx-1, dp);
+    int right = f(idx-2, dp);
+    return dp[idx] = left+ right; 
 }
     int climbStairs(int n) {
         vector<int> dp(n+1, -1);
-       return f(n,dp);
+        return f(n,dp);
     }
 };
