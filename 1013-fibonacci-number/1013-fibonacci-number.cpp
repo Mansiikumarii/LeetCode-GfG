6da4d31cfa6ecbcb96237e3934c523164005d023
Mansi Kumari <162333120+Mansiikumarii@users.noreply.github.com>
2025-08-15 18:02:34 +0530

    Time: 14 ms (21.97%), Space: 7.9 MB (22.07%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,59 +1,16 @@
 class Solution {
 public:
     int fib(int n) {
-        //Itrative
-    //   if(n<0){
-    //     return n;
-    //   }
-    //   if(n==0){
-    //     return 0;
-    //   }  
-    //   int a=0, b=1;
-    //   for(int i=2; i<=n;i++){
-    //     int nextTerm = a+b;
-    //     a=b;
-    //     b=nextTerm;
-    //   }
-    //   return b;
-//recursive
- if(n==0) return 0;
- if(n==1) return 1;
- return fib(n-1)+ fib(n-2) ;
+        //base case
+        if(n==0)
+        return 0;
+        if(n==1)
+        return 1;
 
-//tabular
-//         vector<int>dp(n+1,0);
+        //process
+        int ans= fib(n-1)+ fib(n-2);
 
-//         if(n == 0) return 0;
-//         if(n == 1) return 1;
-
-//         dp[0] = 0;
-//         dp[1] = 1;
-
-//         for(int i = 2;i <= n;i++){
-
-//             dp[i] = dp[i-1]+dp[i-2];
-//         }
-
-//         return dp[n];
-
-
-
-
-
-
-// if(n==0) return 0;
-//    if(n==1) return 1;
-//    int prevprev =0;
-//    int prev = 1;
-//    int ans;
-
-//    for(int i=2;i<=n; i++ ){
-//     ans = prevprev+prev;
-//     prevprev = prev;
-//     prev= ans;
-//        }
-
-// return ans;
-
+        //result
+        return ans;
     }
 };
