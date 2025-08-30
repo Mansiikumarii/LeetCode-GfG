75975cf625a68e24750a29b361882ea1489b0cb2
Mansi Kumari <162333120+Mansiikumarii@users.noreply.github.com>
2025-08-30 14:00:02 +0530

    Time: 4 ms (68.85%), Space: 13.5 MB (84.91%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
@@ -4,6 +4,7 @@ public:
         string result = "";
         for(char c : s){
             if(!result.empty() && result[result.size()-1] == c){
+                //Is the current character (c) the same as the last character we just stored in result?
                 result.pop_back(); // remove last char if duplicate
             } else {
                 result.push_back(c); // keep char
