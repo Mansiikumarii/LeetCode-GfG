775f333b30de712f6a31dc05be4e18d1373abdac
Mansi Kumari <162333120+Mansiikumarii@users.noreply.github.com>
2025-10-26 23:02:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Length of String/length-of-string.cpp
+++ Difficulty: Basic/Length of String/length-of-string.cpp
@@ -4,10 +4,11 @@ class Solution {
   public:
     int lengthString(string &s) {
         // Your code here
-        int count =0;
-        for(int i=0; s[i] != '\0'; i++){
-            count++;
-        }
-        return count;
+        // int count =0;
+        // for(int i=0; s[i] != '\0'; i++){
+        //     count++;
+        // }
+        // return count;
+        return s.size();
     }
 };
