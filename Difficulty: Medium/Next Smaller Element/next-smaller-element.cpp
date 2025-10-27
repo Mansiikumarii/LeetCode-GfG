7da13fe2961a0f8652e931558d6ac50d3b6c599f
Mansi Kumari <162333120+Mansiikumarii@users.noreply.github.com>
2025-10-27 14:57:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Smaller Element/next-smaller-element.cpp
+++ Difficulty: Medium/Next Smaller Element/next-smaller-element.cpp
@@ -4,8 +4,10 @@ class Solution {
         //  code here
         stack<int>s;
         s.push(-1);
-        vector<int>ans(arr.size());
-        for(int i=arr.size()-1; i>=0; i--){
+        int n= arr.size();
+        vector<int>ans(n);
+        
+        for(int i=n-1; i>= 0; i--){
             int curr = arr[i];
             while(s.top() >= curr){
                 s.pop();
