a25ad6598b3aa89f4c90b7d14a4f9a3a02903b23
Mansi Kumari <162333120+Mansiikumarii@users.noreply.github.com>
2025-10-29 10:11:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.cpp
+++ Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.cpp
@@ -16,71 +16,61 @@ class Node {
 
 class Solution {
   public:
-  void traversalLeft(Node* root, vector<int>&ans){
-   if(root ==NULL)
-   return;
-   if(root->left == NULL && root->right == NULL)
-       return;
-   
-   
-   ans.push_back(root->data);
-   
-   if(root->left){
-       traversalLeft(root->left,ans);
-   }
-   else
-   traversalLeft(root->right,ans);
-   
-  }
-  
-  void traversalLeaf(Node* root , vector<int>&ans){
-      //base case
+  void traverseLeft(Node *root, vector<int>&ans){
       if(root == NULL)
       return;
+      
+      if(root->left == NULL && root->right == NULL){
+          return;
+      }
+      ans.push_back(root->data);
+      if(root->left)
+      traverseLeft(root->left,ans);
+      else
+      traverseLeft(root->right,ans);
+  }
+  void traverseLeaf(Node* root, vector<int>& ans){
+      if(root == NULL)
+      return ;
       
       if(root->left == NULL && root->right == NULL){
           ans.push_back(root->data);
           return;
       }
-      traversalLeaf(root->left, ans);
-      traversalLeaf(root->right, ans);
+      traverseLeaf(root->left,ans);
+      traverseLeaf(root->right,ans);
   }
   
-  void traverseRight(Node *root, vector<int>&ans){
-      if((root == NULL) || (root->left == NULL && root->right ==NULL))
+  void traverseRight(Node* root, vector<int>& ans){
+      if(root == NULL)
       return;
       
+      if(root->left == NULL && root->right == NULL){
+          return;
+      }
       if(root->right)
       traverseRight(root->right,ans);
+      
       else
       traverseRight(root->left,ans);
       
-      //wapis jao
+      //bahar aa gaye
       ans.push_back(root->data);
+      
   }
     vector<int> boundaryTraversal(Node *root) {
-        // code here
-      vector<int>ans;
-      if(root == NULL)
-      return ans;
-      
-      ans.push_back(root->data);
-      
-      //left part store karana hai
-      traversalLeft(root->left,ans);
-      
-      //traverse leaf node
-      
-      //left subtree
-      traversalLeaf(root->left, ans);
-      //right subtree
-      traversalLeaf(root->right, ans);
-      
-      //traverse karo right walla part
-      traverseRight(root->right,ans);
-      
-      return ans;
-      
+        vector<int>ans;
+        if(root == NULL)
+        return ans;
+        
+        ans.push_back(root->data);
+    // 1. Left boundary (excluding leaves)
+      traverseLeft(root->left, ans);
+        traverseLeaf(root->left,ans);
+        //right subtree
+        traverseLeaf(root->right,ans);
+        //traverse right part
+        traverseRight(root->right,ans);
+        return ans;
     }
-    
 };
