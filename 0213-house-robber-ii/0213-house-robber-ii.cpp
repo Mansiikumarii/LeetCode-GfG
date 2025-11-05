1fac4b66d925ad5417fe02fe37db1dbf27b18a83
Mansi Kumari <162333120+Mansiikumarii@users.noreply.github.com>
2025-11-05 15:51:45 +0530

    Time: 0 ms (100%), Space: 10.3 MB (66.86%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-int solveTab(vector<int> & nums){
-    int n= nums.size();
+int solve(vector<int>& nums){
+    int n = nums.size();
+    int prev1 = 0;
+    int prev2 = nums[0];
+    for(int i=1; i<n; i++){
+        int incl = prev1 + nums[i];
+        int excl = prev2;
 
-    int prev2 =0;
-    int prev1 = nums[0];
-
-    for(int i=1; i<n;i++){
-        int incl = prev2 + nums[i];
-        int excl = prev1 + 0;
-
-        int ans = max(incl, excl);
-        prev2 = prev1;
-        prev1 = ans;
+        int ans = max(excl, incl);
+        prev1 = prev2;
+        prev2 = ans;
     }
-    return prev1;
+    return prev2;
 }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if(n==1)
-        return nums[0];
 
-        vector<int>first,second;
+        if(n==1)
+            return nums[0];
+        
+        vector<int> first, second;
         for(int i=0; i<n; i++){
             if(i != n-1)
             first.push_back(nums[i]);
@@ -29,6 +28,6 @@ int solveTab(vector<int> & nums){
             if(i != 0)
             second.push_back(nums[i]);
         }
-        return max(solveTab(first), solveTab(second));
+        return max(solve(first), solve(second));
     }
 };
