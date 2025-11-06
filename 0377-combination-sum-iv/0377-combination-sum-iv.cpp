4c2b8d8d11c960c1e4db44e511323386ccf44d4d
Mansi Kumari <162333120+Mansiikumarii@users.noreply.github.com>
2025-11-06 11:16:10 +0530

    Time: 0 ms (100%), Space: 9.1 MB (91.27%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+int solve(vector<int>& num, int tar,vector<int>&dp){
+    //base case
+    if(tar <0)
+    return 0;
+
+    if(tar == 0)
+    return 1;
+
+    if(dp[tar] != -1)
+    return dp[tar];
+
+    int ans=0;
+    for(int i=0; i<num.size(); i++){
+        ans += solve(num, tar-num[i],dp);
+    }
+    dp[tar] = ans;
+    return dp[tar];
+}
+int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target+1, -1);
+        return solve(nums, target, dp);
+    }
+};
