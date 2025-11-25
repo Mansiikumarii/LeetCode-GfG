72fb06da9415873d5c0cee3ab8123c3c5820e4d1
Mansi Kumari <162333120+Mansiikumarii@users.noreply.github.com>
2025-11-25 11:28:49 +0530

    Time: 1 ms (15.23%), Space: 18.6 MB (78.44%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,18 +1,5 @@
 class Solution {
 public:
-
-void markRow(vector<vector<int>>& matrix,int i,int n){
-    for(int j=0; j<n;j++){
-        if(matrix[i][j] != 0)
-        matrix[i][j] = -1;
-    }
-}
-void markCol(vector<vector<int>>& matrix,int j, int m){
-    for(int i=0; i<m; i++){
-        if(matrix[i][j] != 0)
-        matrix[i][j] = -1;
-    }
-}
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n= matrix[0].size();
