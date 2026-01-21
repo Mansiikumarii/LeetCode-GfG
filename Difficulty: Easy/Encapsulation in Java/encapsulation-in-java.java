3441883b8dda7045d19f3398fb2ea93c09499203
Mansi Kumari <162333120+Mansiikumarii@users.noreply.github.com>
2026-01-21 19:06:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Encapsulation in Java/encapsulation-in-java.java
@@ -0,0 +1,27 @@
+// User function Template for Java
+//  Write the complete Person class her
+class Person{
+   
+    private String name;
+    private int age;
+    
+    Person(){
+        name = "Geeks";
+        age =10;
+    }
+    public String getName(){
+        return name;
+    }     
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    
+   
+}
