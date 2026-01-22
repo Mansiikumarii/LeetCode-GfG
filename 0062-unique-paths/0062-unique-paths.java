e0894c892873277744ba36baad5412e863c22711
Mansi Kumari <162333120+Mansiikumarii@users.noreply.github.com>
2026-01-22 18:09:14 +0530

    Time: 0 ms (100%), Space: 42.1 MB (55.11%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,28 @@
+class Solution {
+    int[][] dp;
+    public int solve(int i, int j){
+        
+        if(i==0 && j==0)
+        return 1;
+
+        if(i<0 || j<0)
+        return 0;
+
+        if (dp[i][j] != -1)
+        return dp[i][j];
+        int up = solve(i-1, j);
+        int left = solve(i,j-1);
+
+        dp[i][j] = left+up;
+        return dp[i][j];
+    }
+    public int uniquePaths(int m, int n) {
+        dp = new int[m][n];
+        
+        for(int i=0; i<m; i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return solve( m-1, n-1);
+
+    }
+}
