879782594cfbbcd282bad7629c12a08bb80c3d83
Mansi Kumari <162333120+Mansiikumarii@users.noreply.github.com>
2026-06-14 19:01:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Search in Linked List/search-in-linked-list.py
@@ -0,0 +1,15 @@
+'''
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+'''
+
+class Solution:
+    def searchKey(self, head, key):
+        #Code here
+        if head is None:
+            return False
+        if head.data == key:
+            return True
+        return self.searchKey(head.next,key)
