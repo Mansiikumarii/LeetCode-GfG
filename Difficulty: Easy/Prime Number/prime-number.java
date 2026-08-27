64d1210fffa1e378cf0138fe92c2f632b7945ee1
Mansi Kumari <162333120+Mansiikumarii@users.noreply.github.com>
2026-08-27 09:27:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Prime Number/prime-number.java
+++ Difficulty: Easy/Prime Number/prime-number.java
@@ -4,8 +4,8 @@ class Solution {
         if(n<=1)
         return false;
         
-        for(int i=2; i*i<=n; i++){
-            if(n%i ==0)
+        for(int i=2; i<n; i++){
+            if(n%i == 0)
             return false;
         }
         return true;
